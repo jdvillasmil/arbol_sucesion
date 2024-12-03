b0637648e3f94e076826b99c7d148ca5e2dbc6af
jdvillasmil <jdvillasmil007@gmail.com>
2024-12-03 17:04:07 -0400

    feat: agregar menu interactivo y ajustes en mensajes

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,61 @@
 #include "utils.h"
 using namespace std;
 
+void mostrarMenu() {
+    cout << "\n--- Menu del Sistema de Sucesion ---" << endl;
+    cout << "1. Cargar datos desde el archivo CSV" << endl;
+    cout << "2. Mostrar la linea de sucesion actual" << endl;
+    cout << "3. Reasignar automaticamente al nuevo rey" << endl;
+    cout << "4. Modificar los datos de un nodo" << endl;
+    cout << "5. Salir" << endl;
+    cout << "Seleccione una opcion: ";
+}
+
 int main() {
     ArbolBinario arbol;
+    bool salir = false;
 
-    // Cargar nodos desde el archivo sucesion.csv
-    cargarDesdeCSV("bin/sucesion.csv", arbol);
+    while (!salir) {
+        mostrarMenu();
+        int opcion;
+        cin >> opcion;
+        cin.ignore(); // Limpiar el buffer de entrada
 
-    // Mostrar el arbol cargado
-    cout << "Arbol cargado desde CSV en preorden:" << endl;
-    arbol.mostrarArbol();
+        switch (opcion) {
+            case 1: {
+                cargarDesdeCSV("bin/sucesion.csv", arbol);
+                cout << "Datos cargados correctamente desde sucesion.csv." << endl;
+                break;
+            }
+            case 2: {
+                cout << "Linea de sucesion actual:" << endl;
+                arbol.mostrarLineaSucesion();
+                break;
+            }
+            case 3: {
+                cout << "Reasignando al nuevo rey..." << endl;
+                arbol.reasignarRey();
+                break;
+            }
+            case 4: {
+                cout << "Ingrese el ID del nodo a modificar: ";
+                int id;
+                cin >> id;
+                cin.ignore();
+                arbol.modificarNodo(id);
+                break;
+            }
+            case 5: {
+                cout << "Hasta luego!" << endl;
+                salir = true;
+                break;
+            }
+            default: {
+                cout << "Opcion invalida. Por favor, intente de nuevo." << endl;
+                break;
+            }
+        }
+    }
 
     return 0;
 }
